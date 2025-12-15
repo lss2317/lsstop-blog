a847cbb6f322f0897ff173294f1defdd358760b3
lishusheng <2084447140@qq.com>
2025-12-15 21:24:30 +0800

    first commit

---
+++ blog-lsstop/src/main/java/com/lsstop/BlogLsstopApplication.java
@@ -0,0 +1,13 @@
+package com.lsstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLsstopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogLsstopApplication.class, args);
+    }
+
+}
---
+++ blog-lsstop/src/test/java/com/lsstop/BlogLsstopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLsstopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
